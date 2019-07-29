feb481f18781b6d543e6148326d4bf3e8d644ee4
cathy <jwang4@ualberta.ca>
2019-07-28 22:18:55 -0600

    Initial Commit

--- AddEvent.php
+++ AddEvent.php
@@ -1,14 +1,11 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
-
 if (php_sapi_name() != 'cli') {
     throw new Exception('This application must be run on the command line.');
 }
-
 if (php_sapi_name() != 'cli') {
     throw new Exception('This application must be run on the command line.');
 }
-
 /**
  * Returns an authorized API client.
  * @return Google_Client the authorized client object
@@ -21,7 +18,6 @@ function getClient()
     $client->setAuthConfig('credentials.json');
     $client->setAccessType('offline');
     $client->setPrompt('select_account consent');
-
     // Load previously authorized token from a file, if it exists.
     // The file token.json stores the user's access and refresh tokens, and is
     // created automatically when the authorization flow completes for the first
@@ -31,7 +27,6 @@ function getClient()
         $accessToken = json_decode(file_get_contents($tokenPath), true);
         $client->setAccessToken($accessToken);
     }
-
     // If there is no previous token or it's expired.
     if ($client->isAccessTokenExpired()) {
         // Refresh the token if possible, else fetch a new one.
@@ -43,11 +38,9 @@ function getClient()
             printf("Open the following link in your browser:\n%s\n", $authUrl);
             print 'Enter verification code: ';
             $authCode = trim(fgets(STDIN));
-
             // Exchange authorization code for an access token.
             $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
             $client->setAccessToken($accessToken);
-
             // Check to see if there was an error.
             if (array_key_exists('error', $accessToken)) {
                 throw new Exception(join(', ', $accessToken));
@@ -61,39 +54,33 @@ function getClient()
     }
     return $client;
 }
-
-
 // Get the API client and construct the service object.
 $client = getClient();
 $service = new Google_Service_Calendar($client);
-
 // Print the next 10 events on the user's calendar.
 $calendarId = 'primary';
-
-
 // Refer to the PHP quickstart on how to setup the environment:
 // https://developers.google.com/calendar/quickstart/php
 // Change the scope to Google_Service_Calendar::CALENDAR and delete any stored
 // credentials.
-
 $event = new Google_Service_Calendar_Event(array(
     'summary' => 'Google I/O 2015',
-    'location' => '800 Howard St., San Francisco, CA 94103',
+    'location' => 'University of Alberta, Edmonton, AB',
     'description' => 'A chance to hear more about Google\'s developer products.',
     'start' => array(
-        'dateTime' => '2019-12-28T09:00:00-07:00',
+        'dateTime' => '2019-12-29T09:00:00-07:00',
         'timeZone' => 'America/Los_Angeles',
     ),
     'end' => array(
-        'dateTime' => '2019-12-28T17:00:00-07:00',
+        'dateTime' => '2019-12-29T17:00:00-07:00',
         'timeZone' => 'America/Los_Angeles',
     ),
     'recurrence' => array(
         'RRULE:FREQ=DAILY;COUNT=2'
     ),
     'attendees' => array(
-        array('email' => 'lpage@example.com'),
-        array('email' => 'sbrin@example.com'),
+        array('email' => 'test1@example.com'),
+        array('email' => 'test2@example.com'),
     ),
     'reminders' => array(
         'useDefault' => FALSE,
@@ -103,7 +90,6 @@ $event = new Google_Service_Calendar_Event(array(
         ),
     ),
 ));
-
 $calendarId = 'primary';
 $event = $service->events->insert($calendarId, $event);
 printf('Event created: %s\n', $event->htmlLink);
